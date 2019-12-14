9415aaf71347c791ca89648c504e195020ed7342
Lihzahrd <47698047+Lihzahrd@users.noreply.github.com>
2019-12-13 22:09:43 -0500

    Update modboxcurrent.js

--- BeepBox_files/modboxcurrent.js
+++ BeepBox_files/modboxcurrent.js
@@ -8839,7 +8839,369 @@ var beepbox;
     }
     beepbox.PatternEditor = PatternEditor;
 })(beepbox || (beepbox = {}));
-            this._svg = beepbox.SVG.svg({ style: "background-color: #000000; position: absolute;", height: 128 }, this._boxContainer, this._selectionRect, this._boxHighlight, this._upHighlight, this._downHighlight, this._playhead);
+var beepbox;
+(function (beepbox) {
+    class Box {
+        constructor(channel, x, y, color) {
+            this._text = document.createTextNode("1");
+            this._label = beepbox.SVG.text({ x: 16, y: 23, "font-family": "sans-serif", "font-size": 20, "text-anchor": "middle", "font-weight": "bold", fill: "red" }, this._text);
+            this._rect = beepbox.SVG.rect({ width: 30, height: 30, x: 1, y: 1 });
+            this.container = beepbox.SVG.svg(this._rect, this._label);
+            this._renderedIndex = 1;
+            this._renderedDim = true;
+            this._renderedSelected = false;
+            this._renderedColor = "";
+            this.container.setAttribute("x", "" + (x * 32));
+            this.container.setAttribute("y", "" + (y * 32));
+            this._rect.setAttribute("fill", "#444444");
+            this._label.setAttribute("fill", color);
+        }
+        setSquashed(squashed, y) {
+            if (squashed) {
+                this.container.setAttribute("y", "" + (y * 27));
+                this._rect.setAttribute("height", "" + 25);
+                this._label.setAttribute("y", "" + 21);
+            }
+            else {
+                this.container.setAttribute("y", "" + (y * 32));
+                this._rect.setAttribute("height", "" + 30);
+                this._label.setAttribute("y", "" + 23);
+            }
+        }
+        setIndex(index, dim, selected, y, color) {
+            if (this._renderedIndex != index) {
+                if (!this._renderedSelected && ((index == 0) != (this._renderedIndex == 0))) {
+                    this._rect.setAttribute("fill", (index == 0) ? "#000000" : "#444444");
+                }
+                this._renderedIndex = index;
+                this._text.data = "" + index;
+            }
+            if (this._renderedDim != dim || this._renderedColor != color) {
+                this._renderedDim = dim;
+                if (selected) {
+                    this._label.setAttribute("fill", "#000000");
+                }
+                else {
+                    this._label.setAttribute("fill", color);
+                }
+            }
+            if (this._renderedSelected != selected || this._renderedColor != color) {
+                this._renderedSelected = selected;
+                if (selected) {
+                    this._rect.setAttribute("fill", color);
+                    this._label.setAttribute("fill", "#000000");
+                }
+                else {
+                    this._rect.setAttribute("fill", (this._renderedIndex == 0) ? "#000000" : "#444444");
+                    this._label.setAttribute("fill", color);
+                }
+            }
+            this._renderedColor = color;
+        }
+    }
+    class TrackEditor {
+        constructor(_doc, _songEditor) {
+            this._doc = _doc;
+            this._songEditor = _songEditor;
+            this._barWidth = 32;
+            this._svg = beepbox.SVG.svg({ style: "background-color: #000000; position: absolute;", height: 128 });
+            this._select = beepbox.HTML.select({ className: "trackSelectBox", style: "width: 32px; height: 32px; background: none; border: none; appearance: none; color: transparent; position: absolute;" });
+            this.container = beepbox.HTML.div({ style: "height: 128px; position: relative; overflow:hidden;" }, this._svg, this._select);
+            this._boxContainer = beepbox.SVG.g();
+            this._playhead = beepbox.SVG.rect({ fill: "white", x: 0, y: 0, width: 4, height: 128 });
+            this._boxHighlight = beepbox.SVG.rect({ fill: "none", stroke: "white", "stroke-width": 2, "pointer-events": "none", x: 1, y: 1, width: 30, height: 30 });
+            this._upHighlight = beepbox.SVG.path({ fill: "black", stroke: "black", "stroke-width": 1, "pointer-events": "none" });
+            this._downHighlight = beepbox.SVG.path({ fill: "black", stroke: "black", "stroke-width": 1, "pointer-events": "none" });
+            this._grid = [];
+            this._mouseX = 0;
+            this._mouseY = 0;
+            this._pattern = null;
+            this._mouseOver = false;
+            this._digits = "";
+            this._editorHeight = 128;
+            this._channelHeight = 32;
+            this._renderedChannelCount = 0;
+            this._renderedBarCount = 0;
+            this._renderedPatternCount = 0;
+            this._renderedPlayhead = -1;
+            this._renderedSquashed = false;
+            this._changePattern = null;
+            this._whenSelectChanged = () => {
+                this._setPattern(this._select.selectedIndex);
+            };
+            this._animatePlayhead = (timestamp) => {
+                const playhead = (this._barWidth * this._doc.synth.playhead - 2);
+                if (this._renderedPlayhead != playhead) {
+                    this._renderedPlayhead = playhead;
+                    this._playhead.setAttribute("x", "" + playhead);
+                }
+                window.requestAnimationFrame(this._animatePlayhead);
+            };
+            this._whenMouseOver = (event) => {
+                if (this._mouseOver)
+                    return;
+                this._mouseOver = true;
+            };
+            this._whenMouseOut = (event) => {
+                if (!this._mouseOver)
+                    return;
+                this._mouseOver = false;
+            };
+            this._whenMousePressed = (event) => {
+                event.preventDefault();
+                const boundingRect = this._svg.getBoundingClientRect();
+                this._mouseX = (event.clientX || event.pageX) - boundingRect.left;
+                this._mouseY = (event.clientY || event.pageY) - boundingRect.top;
+                const channel = Math.floor(Math.min(this._doc.song.getChannelCount() - 1, Math.max(0, this._mouseY / this._channelHeight)));
+                const bar = Math.floor(Math.min(this._doc.song.barCount - 1, Math.max(0, this._mouseX / this._barWidth)));
+                if (this._doc.channel == channel && this._doc.bar == bar) {
+                    const up = (this._mouseY % this._channelHeight) < this._channelHeight / 2;
+                    const patternCount = this._doc.song.patternsPerChannel;
+                    this._setPattern((this._doc.song.channels[channel].bars[bar] + (up ? 1 : patternCount)) % (patternCount + 1));
+                }
+                else {
+                    this._setChannelBar(channel, bar);
+                }
+            };
+            this._whenMouseMoved = (event) => {
+                const boundingRect = this._svg.getBoundingClientRect();
+                this._mouseX = (event.clientX || event.pageX) - boundingRect.left;
+                this._mouseY = (event.clientY || event.pageY) - boundingRect.top;
+                this._updatePreview();
+            };
+            this._whenMouseReleased = (event) => {
+            };
+            this._svg.appendChild(this._boxContainer);
+            this._svg.appendChild(this._boxHighlight);
+            this._svg.appendChild(this._upHighlight);
+            this._svg.appendChild(this._downHighlight);
+            this._svg.appendChild(this._playhead);
+            window.requestAnimationFrame(this._animatePlayhead);
+            this._svg.addEventListener("mousedown", this._whenMousePressed);
+            document.addEventListener("mousemove", this._whenMouseMoved);
+            document.addEventListener("mouseup", this._whenMouseReleased);
+            this._svg.addEventListener("mouseover", this._whenMouseOver);
+            this._svg.addEventListener("mouseout", this._whenMouseOut);
+            this._select.addEventListener("change", this._whenSelectChanged);
+        }
+        _setChannelBar(channel, bar) {
+            new beepbox.ChangeChannelBar(this._doc, channel, bar);
+            this._digits = "";
+            this._doc.forgetLastChange();
+        }
+        _setPattern(pattern) {
+            const currentValue = this._doc.song.channels[this._doc.channel].bars[this._doc.bar];
+            const canReplaceLastChange = this._doc.lastChangeWas(this._changePattern);
+            const oldValue = canReplaceLastChange ? this._changePattern.oldValue : currentValue;
+            if (pattern != currentValue) {
+                this._changePattern = new beepbox.ChangePattern(this._doc, oldValue, pattern);
+                this._doc.record(this._changePattern, canReplaceLastChange);
+            }
+        }
+        onKeyPressed(event) {
+            switch (event.keyCode) {
+                case 38:
+                    this._setChannelBar((this._doc.channel - 1 + this._doc.song.getChannelCount()) % this._doc.song.getChannelCount(), this._doc.bar);
+                    event.preventDefault();
+                    break;
+                case 40:
+                    this._setChannelBar((this._doc.channel + 1) % this._doc.song.getChannelCount(), this._doc.bar);
+                    event.preventDefault();
+                    break;
+                case 37:
+                    this._setChannelBar(this._doc.channel, (this._doc.bar + this._doc.song.barCount - 1) % this._doc.song.barCount);
+                    event.preventDefault();
+                    break;
+                case 39:
+                    this._setChannelBar(this._doc.channel, (this._doc.bar + 1) % this._doc.song.barCount);
+                    event.preventDefault();
+                    break;
+                case 48:
+                    this._nextDigit("0");
+                    event.preventDefault();
+                    break;
+                case 49:
+                    this._nextDigit("1");
+                    event.preventDefault();
+                    break;
+                case 50:
+                    this._nextDigit("2");
+                    event.preventDefault();
+                    break;
+                case 51:
+                    this._nextDigit("3");
+                    event.preventDefault();
+                    break;
+                case 52:
+                    this._nextDigit("4");
+                    event.preventDefault();
+                    break;
+                case 53:
+                    this._nextDigit("5");
+                    event.preventDefault();
+                    break;
+                case 54:
+                    this._nextDigit("6");
+                    event.preventDefault();
+                    break;
+                case 55:
+                    this._nextDigit("7");
+                    event.preventDefault();
+                    break;
+                case 56:
+                    this._nextDigit("8");
+                    event.preventDefault();
+                    break;
+                case 57:
+                    this._nextDigit("9");
+                    event.preventDefault();
+                    break;
+                default:
+                    this._digits = "";
+                    break;
+            }
+        }
+        _nextDigit(digit) {
+            this._digits += digit;
+            let parsed = parseInt(this._digits);
+            if (parsed <= this._doc.song.patternsPerChannel) {
+                this._setPattern(parsed);
+                return;
+            }
+            this._digits = digit;
+            parsed = parseInt(this._digits);
+            if (parsed <= this._doc.song.patternsPerChannel) {
+                this._setPattern(parsed);
+                return;
+            }
+            this._digits = "";
+        }
+        _updatePreview() {
+            let channel = Math.floor(Math.min(this._doc.song.getChannelCount() - 1, Math.max(0, this._mouseY / this._channelHeight)));
+            let bar = Math.floor(Math.min(this._doc.song.barCount - 1, Math.max(0, this._mouseX / this._barWidth)));
+            const wideScreen = window.innerWidth > 700;
+            if (!wideScreen) {
+                bar = this._doc.bar;
+                channel = this._doc.channel;
+            }
+            const selected = (bar == this._doc.bar && channel == this._doc.channel);
+            if (this._mouseOver && !selected) {
+                this._boxHighlight.setAttribute("x", "" + (1 + this._barWidth * bar));
+                this._boxHighlight.setAttribute("y", "" + (1 + (this._channelHeight * channel)));
+                this._boxHighlight.setAttribute("height", "" + (this._channelHeight - 2));
+                this._boxHighlight.style.visibility = "visible";
+            }
+            else {
+                this._boxHighlight.style.visibility = "hidden";
+            }
+            if ((this._mouseOver || !wideScreen) && selected) {
+                const up = (this._mouseY % this._channelHeight) < this._channelHeight / 2;
+                const center = this._barWidth * (bar + 0.8);
+                const middle = this._channelHeight * (channel + 0.5);
+                const base = this._channelHeight * 0.1;
+                const tip = this._channelHeight * 0.4;
+                const width = this._channelHeight * 0.175;
+                this._upHighlight.setAttribute("fill", up && wideScreen ? "#fff" : "#000");
+                this._downHighlight.setAttribute("fill", !up && wideScreen ? "#fff" : "#000");
+                this._upHighlight.setAttribute("d", `M ${center} ${middle - tip} L ${center + width} ${middle - base} L ${center - width} ${middle - base} z`);
+                this._downHighlight.setAttribute("d", `M ${center} ${middle + tip} L ${center + width} ${middle + base} L ${center - width} ${middle + base} z`);
+                this._upHighlight.style.visibility = "visible";
+                this._downHighlight.style.visibility = "visible";
+            }
+            else {
+                this._upHighlight.style.visibility = "hidden";
+                this._downHighlight.style.visibility = "hidden";
+            }
+            this._select.style.left = (this._barWidth * this._doc.bar) + "px";
+            this._select.style.top = (this._channelHeight * this._doc.channel) + "px";
+            this._select.style.height = this._channelHeight + "px";
+            const patternCount = this._doc.song.patternsPerChannel + 1;
+            for (let i = this._renderedPatternCount; i < patternCount; i++) {
+                this._select.appendChild(beepbox.HTML.option({ value: i }, i));
+            }
+            for (let i = patternCount; i < this._renderedPatternCount; i++) {
+                this._select.removeChild(this._select.lastChild);
+            }
+            this._renderedPatternCount = patternCount;
+            const selectedPattern = this._doc.song.channels[this._doc.channel].bars[this._doc.bar];
+            if (this._select.selectedIndex != selectedPattern)
+                this._select.selectedIndex = selectedPattern;
+        }
+        render() {
+            this._pattern = this._doc.getCurrentPattern();
+            const wideScreen = window.innerWidth > 700;
+            const squashed = !wideScreen || this._doc.song.getChannelCount() > 4 || (this._doc.song.barCount > this._doc.trackVisibleBars && this._doc.song.getChannelCount() > 3);
+            this._channelHeight = squashed ? 27 : 32;
+            if (this._renderedChannelCount != this._doc.song.getChannelCount()) {
+                for (let y = this._renderedChannelCount; y < this._doc.song.getChannelCount(); y++) {
+                    this._grid[y] = [];
+                    for (let x = 0; x < this._renderedBarCount; x++) {
+                        const box = new Box(y, x, y, this._doc.getChannelColorDim(y));
+                        box.setSquashed(squashed, y);
+                        this._boxContainer.appendChild(box.container);
+                        this._grid[y][x] = box;
+                    }
+                }
+                for (let y = this._doc.song.getChannelCount(); y < this._renderedChannelCount; y++) {
+                    for (let x = 0; x < this._renderedBarCount; x++) {
+                        this._boxContainer.removeChild(this._grid[y][x].container);
+                    }
+                }
+                this._grid.length = this._doc.song.getChannelCount();
+            }
+            if (this._renderedBarCount != this._doc.song.barCount) {
+                for (let y = 0; y < this._doc.song.getChannelCount(); y++) {
+                    for (let x = this._renderedBarCount; x < this._doc.song.barCount; x++) {
+                        const box = new Box(y, x, y, this._doc.getChannelColorDim(y));
+                        box.setSquashed(squashed, y);
+                        this._boxContainer.appendChild(box.container);
+                        this._grid[y][x] = box;
+                    }
+                    for (let x = this._doc.song.barCount; x < this._renderedBarCount; x++) {
+                        this._boxContainer.removeChild(this._grid[y][x].container);
+                    }
+                    this._grid[y].length = this._doc.song.barCount;
+                }
+                this._renderedBarCount = this._doc.song.barCount;
+                const editorWidth = 32 * this._doc.song.barCount;
+                this.container.style.width = editorWidth + "px";
+                this._svg.setAttribute("width", editorWidth + "");
+            }
+            if (this._renderedSquashed != squashed) {
+                for (let y = 0; y < this._doc.song.getChannelCount(); y++) {
+                    for (let x = 0; x < this._renderedBarCount; x++) {
+                        this._grid[y][x].setSquashed(squashed, y);
+                    }
+                }
+            }
+            if (this._renderedSquashed != squashed || this._renderedChannelCount != this._doc.song.getChannelCount()) {
+                this._renderedSquashed = squashed;
+                this._renderedChannelCount = this._doc.song.getChannelCount();
+                this._editorHeight = this._doc.song.getChannelCount() * this._channelHeight;
+                this._svg.setAttribute("height", "" + this._editorHeight);
+                this._playhead.setAttribute("height", "" + this._editorHeight);
+                this.container.style.height = this._editorHeight + "px";
+            }
+            for (let j = 0; j < this._doc.song.getChannelCount(); j++) {
+                for (let i = 0; i < this._renderedBarCount; i++) {
+                    const pattern = this._doc.song.getPattern(j, i);
+                    const selected = (i == this._doc.bar && j == this._doc.channel);
+                    const dim = (pattern == null || pattern.notes.length == 0);
+                    const box = this._grid[j][i];
+                    if (i < this._doc.song.barCount) {
+                        box.setIndex(this._doc.song.channels[j].bars[i], dim, selected, j, dim && !selected ? this._doc.getChannelColorDim(j) : this._doc.getChannelColorBright(j));
+                        box.container.style.visibility = "visible";
+                    }
+                    else {
+                        box.container.style.visibility = "hidden";
+                    }
+                }
+            }
+            this._updatePreview();
+        }
+    }
+    beepbox.TrackEditor = TrackEditor;
+})(beepbox || (beepbox = {}));
 var beepbox;
 (function (beepbox) {
     class LoopEditor {
